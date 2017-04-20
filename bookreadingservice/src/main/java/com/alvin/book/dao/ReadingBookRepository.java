37fafaf5e8427f9c134ffb472e4a29482466f5b8
haoqiang <haoqiang@oneapm.com>
2017-04-20 15:46:00 +0800

    add README.md and exclude old version sl4j

--- bookreadingservice/src/main/java/com/alvin/book/dao/ReadingBookRepository.java
+++ bookreadingservice/src/main/java/com/alvin/book/dao/ReadingBookRepository.java
@@ -8,6 +8,9 @@ import java.util.List;
 
 /**
  * Created by Administrator on 2017/4/18.
+ *
+ * QueryDslPredicate 简单实用方法
+ * http://www.jianshu.com/p/2b68af9aa0f5
  */
 public interface ReadingBookRepository extends JpaRepository<Book, Long>, QueryDslPredicateExecutor<Book> {
     List<Book> findByReader(String reader);
