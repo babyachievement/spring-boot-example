dc388619f83daef709cffc42543feebd7462c37c
haoqiang <haoqiang@oneapm.com>
2017-04-21 13:41:31 +0800

    modify alvincommon and bookreadingweb compiler level

--- my-schedule/src/test/java/com/alvin/web/HomeControllerTest.java
+++ my-schedule/src/test/java/com/alvin/web/HomeControllerTest.java
@@ -1,12 +1,12 @@
 package com.alvin.web;
 
-import org.junit.Before;
 import org.junit.Test;
-import org.mockito.InjectMocks;
-import org.mockito.MockitoAnnotations;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -16,24 +16,44 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  * If you need to configure elements of the auto-configuration (for example when servlet filters should be applied)
  * you can use attributes in the @AutoConfigureMockMvc annotation.
  */
+@RunWith(SpringRunner.class)
+@WebMvcTest(HomeController.class)
+/**
+ * If you need to configure elements of the auto-configuration (for example when servlet filters should be applied)
+ * you can use attributes in the @AutoConfigureMockMvc annotation.
+ */
 public class HomeControllerTest {
 
-    private MockMvc mockMvc;
+    @Autowired
+    private MockMvc mvc;
 
-    @InjectMocks
-    private HomeController homeController;
-
-    @Before
-    public void setup() {
-        MockitoAnnotations.initMocks(this);
-        mockMvc = MockMvcBuilders.standaloneSetup(homeController).build();
-    }
     @Test
     public void indexTest() throws Exception {
-        this.mockMvc.perform(get("/")
+        this.mvc.perform(get("/")
                 .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(content().string("Welcome"));
     }
 
 }
+//public class HomeControllerTest {
+//
+//    private MockMvc mockMvc;
+//
+//    @InjectMocks
+//    private HomeController homeController;
+//
+//    @Before
+//    public void setup() {
+//        MockitoAnnotations.initMocks(this);
+//        mockMvc = MockMvcBuilders.standaloneSetup(homeController).build();
+//    }
+//    @Test
+//    public void indexTest() throws Exception {
+//        this.mockMvc.perform(get("/")
+//                .accept(MediaType.APPLICATION_JSON))
+//                .andExpect(status().isOk())
+//                .andExpect(content().string("Welcome"));
+//    }
+//
+//}
